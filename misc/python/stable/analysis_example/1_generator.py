5bd8c57675b5fc576a0932819cf113056c9937ab
Mindaugas Šarpis <mindaugas.sarpis@cern.ch>
2025-10-31 15:17:54 +0200

    analysis and fit example

--- misc/python/stable/analysis_example/1_generator.py
+++ misc/python/stable/analysis_example/1_generator.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 # Parameters for the Gaussian signal
 signal_mean = 5.0  # Center of Gaussian
 signal_std_dev = 1.0  # Standard deviation of Gaussian
-num_sig_points = 10000  # Number of data points for the signal
+num_sig_points = 75000  # Number of data points for the signal
 
 # Parameters for the exponential background
 num_background_points = 100000  # Number of data points for the background
@@ -19,18 +19,23 @@ exponential_background = np.random.exponential(scale=2.0, size=num_background_po
 # Combine signal and background data
 all_data = np.concatenate([gaussian_signal, exponential_background])
 
-# Plot the combined data
-plt.hist(
-    exponential_background,
-    bins=50,
-    alpha=0.5,
-    label="Background (exp)",
-    color="tab:blue",
-)
+# --- Plot ---
+plt.hist(exponential_background, bins=50, alpha=0.5, label="Background (exp)", color="tab:blue", range=(0, 10))
+plt.hist(gaussian_signal, bins=50, alpha=0.5, label="Signal (gauss)", color="tab:orange", range=(0, 10))
+plt.hist(all_data, bins=50, histtype="step", linewidth=2, color="black", label="Total", range=(0, 10))
 
-plt.hist(
-    gaussian_signal, bins=50, alpha=0.5, label="Signal (gauss)", color="tab:orange"
-)
-
-plt.hist(all_data, bins=50, histtype="step", linewidth=2, color="black", label="Total")
+plt.xlabel("x")
+plt.ylabel("Entries")
+plt.legend()
 plt.show()
+
+
+
+with open("sample.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["data"])
+    writer.writerows([[x] for x in all_data])
+    f.close()   
+    
+print(f"Data saved to sample.csv")
+
